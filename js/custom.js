b2841eb444a7c97a73603dec761e0342f37f4b62
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2021-08-03 00:21:45 +0000

    third

--- js/custom.js
+++ js/custom.js
@@ -3,23 +3,51 @@
 
 
 $.ajax({
-    url: 'https://newsapi.org/v2/everything?q=tesla&from=2021-06-29&sortBy=publishedAt&apiKey=eee2311613bf4b669a244d9abb347b07',
-    success:function(data){
-      console.log(data);
-      var i;
-          for (i = 0; i < data.articles.length; i++) {
-            var x = data.articles[i]
-            document.getElementById('objects').innerHTML +=
-              '<div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3"> ' +
-              '<div class="card mt-3 ml-5" style="width: 20rem;">' +
-              '<div class="card-body">' +
-              '<class="card-text">' + '<h4>' + x.title + '</h4>' + '<br>' + '<p>' + x.description + '</p>' + '<br>' + 'Color: ' + x.author + '<br>' +
-              '</div>' +
-              '</div>' +
-              '</div>';
-          } //for loop
-    },
-    error:function(){
-        alert('An error was encountered.');
-    }
+  url: 'https://newsapi.org/v2/everything?q=tesla&from=2021-07-02&sortBy=publishedAt&apiKey=eee2311613bf4b669a244d9abb347b07',
+  type: 'GET',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data.articles);
+
+    // document.getElementById('result').innerHTML = '';
+
+    var i;
+    for (i = 0; i < data.articles.length; i++) {
+      var x = data.articles[i]
+      document.getElementById('objects').innerHTML +=
+        '<div class="articles col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6"> ' +
+        '<div class="card mt-3 ml-5" style="width: 45rem;">' +
+        '<div class="card-body">' + '<img src="' + x.urlToImage + '">' +
+        '<class="card-text">' + '<h4>' + x.title + '</h4>' + '<br>' + '<p>' + x.description + '</p>' + '<br>' + '<p> Author: ' + x.author + '</p><br>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+    } //for loop
+  },
+  error: function() {
+    alert('An error was encountered.');
+  }
 });
+
+$('.about-popup').click(function(e) {
+  popup();
+  $('.about-popup').toggleClass('fullscreen');
+});
+
+function popup() {
+  // popup down
+  if ($('.about-popup').hasClass('popup-hover')) {
+    $('.about-popup').removeClass('popup-hover');
+    $('.popup-before').addClass('hide');
+    $('.popup-before').fadeOut(100,
+      function() {
+        $('.popup-after').fadeIn(1500);
+      });
+  } else { // popup up
+    $('.about-popup').addClass('popup-hover');
+    $('.popup-after').fadeOut(100,
+      function() {
+        $('.popup-before').fadeIn(1200);
+      });
+  }
+};
